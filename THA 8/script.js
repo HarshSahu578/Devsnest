3c14b4f34a4a6d17a9ff63a2c2ab3dac37859c23
Harsh Sahu <83756880+HarshSahu578@users.noreply.github.com>
2021-06-21 22:08:07 +0530

    Add files via upload

---
+++ THA 8/script.js
@@ -0,0 +1,7 @@
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(element => {
+    element.addEventListener('click', ()=> {
+        element.classList.toggle('sq-color');
+    })
+});
